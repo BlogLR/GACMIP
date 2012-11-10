3a37d2c4761a43cd53c472b82be7675e75923e8c
leandro <leandro@leandro-MS-1454>
2012-11-10 14:44:32 -0300

    Criado os metodos addRelacao e getRelacao na class BancoDados

--- GeradorDeCodigo/src/xml/BancoDados.java
+++ GeradorDeCodigo/src/xml/BancoDados.java
@@ -3,13 +3,14 @@ package xml;
 import org.xml.sax.Attributes;
 
 /**
- * 
+ *
  * @author Leandro Rolim
  */
 public class BancoDados {
 
     private String nome = "";
     private Tabela[] tbl = new Tabela[0];
+    private Relacao[] rel = new Relacao[0];
 
     public BancoDados(Attributes attr) {
         this.setAttr(attr);
@@ -28,10 +29,23 @@ public class BancoDados {
     public void addTabela(Attributes attr) {
         Tabela[] tmp = this.tbl;
         this.tbl = new Tabela[tmp.length + 1];
-        
+
         System.arraycopy(tmp, 0, this.tbl, 0, tmp.length);
-        
+
         this.tbl[tmp.length] = new Tabela(attr);
+    }
+
+    public void addRelacao(Attributes attr) {
+        Relacao[] tmp = this.rel;
+        this.rel = new Relacao[tmp.length + 1];
+
+        System.arraycopy(tmp, 0, this.rel, 0, tmp.length);
+
+        this.rel[tmp.length] = new Relacao(attr);
+    }
+
+    public Relacao[] getRelacao() {
+        return this.rel;
     }
 
     public Tabela[] getTabela() {
